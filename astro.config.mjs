4cafa723e17a51520acee749c472a8f5946381c3
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2026-01-09 13:25:21 +0000

    Fixed bad canonical url.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from '@astrojs/react';
 
+const SITE_URL = process.env.VERCEL_URL 
+  ? `https://${process.env.VERCEL_URL}`
+  : 'https://davidjbarrios.com';
+
 // https://astro.build/config
 export default defineConfig({
+  site: SITE_URL,
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
