138a30a3b1d2264f9b356c91b1a01d3ff76ec6ef
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2026-01-14 19:51:19 +0000

    Fixed incorrect json structure accessing on content. Removed min height screen css rule.

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ export interface PageInfo {
   pageName: string;
   pageTitle: string;
   pageType: 'videoGridPage' | 'blockPage';
-  textpageContent: any[];
+  content: { textpageContent: any[]; };
   mediaItems: any[];
   pageDescription: string;
 }
